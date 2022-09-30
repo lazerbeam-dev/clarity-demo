1401cdb8fac49bebf92c70bd9f0b45b2940e3db6
Jack Tomlinson <jackiet333@protonmail.com>
2022-09-29 21:05:49 -0500

    all tests failing

--- contract/vault-demo/tests/vault_test.ts
+++ contract/vault-demo/tests/vault_test.ts
@@ -1,26 +1,25 @@
+import {
+    Chain,
+    Clarinet,
+    Account,
+    Tx,
+    types,
+  } from 'https://deno.land/x/clarinet@v0.33.0/index.ts'
 
-import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.31.0/index.ts';
-import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+  Clarinet.test({
+    name: 'Ensure that contract can only be transferred by owner',
+    fn(chain: Chain, accounts: Map<string, Account>) {
+      const wallet_1 = accounts.get('wallet_1')
+      const wallet_2 = accounts.get('wallet_2')
 
-Clarinet.test({
-    name: "Ensure that <...>",
-    async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+      const block = chain.mineBlock([
+        Tx.contractCall('vault', 'transfer-contract', [types.principal(wallet_1?.address ?? "")], wallet_2?.address ?? ""),
+        Tx.contractCall('vault', 'transfer-contract', [types.principal(wallet_2?.address ?? "")], wallet_1?.address ?? ""),
+        Tx.contractCall('vault', 'transfer-contract', [types.principal(wallet_1?.address ?? "")], wallet_2?.address ?? ""),   
+      ])
+
+      console.log(block.receipts)
+      // ...
     },
-});
+  })
